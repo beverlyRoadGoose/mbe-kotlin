59443a22c68638f92ee186a4d265dc3bafd8f93b
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2018-11-19 20:25:02 +0100

    define pom file and application entry point

---
+++ src/main/kotlin/me/tobiadeyinka/movies/Application.kt
@@ -0,0 +1,14 @@
+package me.tobiadeyinka.movies
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@SpringBootApplication
+@EnableSwagger2
+class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java)
+}
